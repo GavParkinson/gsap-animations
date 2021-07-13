e0e435419640c88ae10dcb3cc117e5ba3f7c0652
Gavin Parkinson <gavinparkinson123@hotmail.com>
2021-07-13 21:54:46 +0100

    Added slide animation

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-slide',
@@ -8,8 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class SlideComponent implements OnInit {
 
   constructor() { }
+  ngOnInit() {
+    // Register the ScrollTrigger with gsap
+    gsap.registerPlugin(ScrollTrigger);
 
-  ngOnInit(): void {
+    //Loop over all the sections and set animations
+    gsap.utils.toArray("section").forEach((section: any, i) => {
+
+      // Give the backgrounds some random images
+      section.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
+
+
+      gsap.to(section, {
+        scrollTrigger: {
+          // Trigger the animation as soon as the section comes into view
+          trigger: section,
+
+          // Pin the section to give a feeling like slide for next section
+          pin: true,
+
+          // Remove the extra pin space added by default
+          pinSpacing: false
+        }
+      });
+    });
   }
-
 }
